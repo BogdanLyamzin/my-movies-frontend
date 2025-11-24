7d78269eb406931dbb459076d47bff4467870430
BohdanLiamzin <bohdanliamzin@gmail.com>
2025-11-24 11:10:08 +0100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navigation from "./pages/Navigation";
+
+import "./styles/style.css";
+
+function App() {
+  return (
+    <>
+     <Navigation /> 
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Navigation.jsx
@@ -0,0 +1,19 @@
+import {Routes, Route} from "react-router-dom";
+
+import HomePage from "./HomePage/HomePage";
+import LoginPage from "./LoginPage/LoginPage";
+import RegisterPage from "./RegisterPage/RegisterPage";
+import NotFoundPage from "./NotFoundPage/NotFoundPage";
+
+const Navigation = ()=> {
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+    )
+}
+
+export default Navigation;
