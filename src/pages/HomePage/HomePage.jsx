210bf6180a4fc24e5118b947377853f1d1b0c742
BohdanLiamzin <bohdanliamzin@gmail.com>
2025-11-25 08:06:18 +0100

    fix reset.css & add shared component button

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,12 @@
+import Button from "../../shared/components/Button/Button";
+
 const HomePage = ()=> {
-    return <h1>Home page</h1>
+    return (
+        <>
+        <h1>Home page</h1>
+        <Button>Add new item</Button>
+        </>
+    )
 }
 
 export default HomePage;
